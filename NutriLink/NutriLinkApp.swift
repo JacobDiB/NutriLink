dd91a4704fc6565fa23e7595aa566f91737ec1df
Jacob DiBenedetto <jacobdib54@gmail.com>
2025-11-05 13:30:18 -0500

    Initial Commit

---
+++ NutriLink/NutriLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriLinkApp.swift
+//  NutriLink
+//
+//  Created by CS3714 on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct NutriLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
