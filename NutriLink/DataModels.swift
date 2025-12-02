e19c2ad2d929f83ff0f4e9cf83850e4411156875
JacobDiB <102424281+JacobDiB@users.noreply.github.com>
2025-12-02 14:08:21 -0500

    Update DataModels.swift

--- NutriLink/DataModels.swift
+++ NutriLink/DataModels.swift
@@ -4,9 +4,11 @@
 //
 //  Created by Jack Micklus on 11/22/25.
 //
+
 import Foundation
 import SwiftData
 
+// Stores login info, goals, and coach-related data for a single user
 @Model
 class UserAccount {
 
@@ -15,6 +17,11 @@ class UserAccount {
     var username: String
     var dailyLogs: [DailyLog]
     var goalCalories: String
+    var goalProtein: String
+    var goalCarbs: String
+    var goalFat: String
+    var mealPlan: String
+    var coachNotes: String
     var coach: CoachAccount?
 
     init(email: String,
@@ -22,6 +29,11 @@ class UserAccount {
          username: String,
          dailyLogs: [DailyLog] = [],
          goalCalories: String,
+         goalProtein: String = "",
+         goalCarbs: String = "",
+         goalFat: String = "",
+         mealPlan: String = "",
+         coachNotes: String = "",
          coach: CoachAccount? = nil) {
         
         self.email = email
@@ -29,10 +41,16 @@ class UserAccount {
         self.username = username
         self.dailyLogs = dailyLogs
         self.goalCalories = goalCalories
+        self.goalProtein = goalProtein
+        self.goalCarbs = goalCarbs
+        self.goalFat = goalFat
+        self.mealPlan = mealPlan
+        self.coachNotes = coachNotes
         self.coach = coach
     }
 }
 
+// Represents a nutrition coach and the users they work with
 @Model
 class CoachAccount {
 
@@ -56,15 +74,48 @@ class CoachAccount {
     }
 }
 
+// Aggregates calorie and food data for a single day
 @Model
 class DailyLog {
     var date: Date
     var calories: Int
-    
-    init(date: Date, calories: Int) {
-            self.date = date
-            self.calories = calories
-        }
+
+    @Relationship(deleteRule: .cascade, inverse: \FoodEntry.dailyLog)
+    var foodEntries: [FoodEntry]
+
+    init(date: Date, calories: Int, foodEntries: [FoodEntry] = []) {
+        self.date = date
+        self.calories = calories
+        self.foodEntries = foodEntries
+    }
 }
 
+// Individual food item logged by the user, including macros
+@Model
+class FoodEntry {
+    var name: String
+    var calories: Int
+    var protein: Double
+    var carbs: Double
+    var fat: Double
+    var date: Date
+
+    @Relationship var dailyLog: DailyLog?
+
+    init(name: String,
+         calories: Int,
+         protein: Double,
+         carbs: Double,
+         fat: Double,
+         date: Date,
+         dailyLog: DailyLog? = nil) {
+        self.name = name
+        self.calories = calories
+        self.protein = protein
+        self.carbs = carbs
+        self.fat = fat
+        self.date = date
+        self.dailyLog = dailyLog
+    }
+}
 
